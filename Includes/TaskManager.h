f7ce905dadbd313f192331856c13c9d8c31b2aaa
David St-Cyr <st-cyrdavid@hotmail.com>
2023-10-21 20:47:53 -0400

    Allow task manager to get a state per task

--- Includes/TaskManager.h
+++ Includes/TaskManager.h
@@ -1,24 +1,134 @@
 #pragma once
+#include <map>
+#include <typeindex>
 #include <functional>
+#include <memory>
+#include <list>
 #include <vector>
+
+class CTaskState
+{};
+
+class CTaskCallbackBase
+{
+private:
+    virtual bool InternalCall(float dt, CTaskState* e) = 0;
+
+public:
+    virtual ~CTaskCallbackBase() = default;
+    bool Invoke(float dt, CTaskState* e);
+    virtual bool Update(float dt) = 0;
+};
+
+template<typename TObject, typename TState>
+class CTaskCallback : public CTaskCallbackBase
+{
+private:
+    using CallbackFunction = bool (TObject::*)(float, TState*);
+
+    TObject* m_userObject;
+    TState* m_stateObject;
+    CallbackFunction m_callback;
+
+    bool InternalCall(float dt, CTaskState* e) override;
+
+public:
+    virtual ~CTaskCallback() override = default;
+    CTaskCallback(TObject* userObject, CallbackFunction callback);
+    bool Update(float dt) override;
+};
 
 class CTaskManager
 {
 public:
-    bool HasTasks() const;
-    void Update(float dt);
-    void Clear();
+    using TaskCallbackList = std::list<CTaskCallbackBase*>;
 
-    template<typename TObject>
-    inline void Add(TObject* userObject, bool(TObject::* callback)(float));
+    void Clear();
+    void Update(float dt);
+    bool HasTasks() const;
+
+    template<typename TObject, typename TState>
+    void Add(TObject* userObject, bool(TObject::* callback)(float, TState*));
 
 private:
-    std::vector<std::function<bool(float)>> m_tasks;
+    TaskCallbackList m_taskList;
+    TaskCallbackList m_taskToDelete;
 };
 
-template<typename TObject>
-inline void CTaskManager::Add(TObject* userObject, bool(TObject::* callback)(float))
+template<typename TObject, typename TState>
+inline bool CTaskCallback<TObject, TState>::InternalCall(float dt, CTaskState* e)
 {
-    auto func = std::bind(callback, userObject, std::placeholders::_1);
-    m_tasks.push_back(func);
+    auto r = std::invoke(m_callback, m_userObject, dt, static_cast<TState*>(e));
+    return r;
 }
+
+template<typename TObject, typename TState>
+inline CTaskCallback<TObject, TState>::CTaskCallback(TObject* userObject, CallbackFunction callback)
+{
+    m_userObject = userObject;
+    m_stateObject = new TState();
+    m_callback = callback;
+}
+
+template<typename TObject, typename TState>
+inline bool CTaskCallback<TObject, TState>::Update(float dt)
+{
+    return InternalCall(dt, m_stateObject);
+}
+
+template<typename TObject, typename TState>
+inline void CTaskManager::Add(TObject* userObject, bool(TObject::* callback)(float, TState*))
+{
+    auto newTask = new CTaskCallback<TObject, TState>(userObject, callback);
+    m_taskList.push_back(newTask);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//class CTaskManager
+//{
+//public:
+//    bool HasTasks() const;
+//    void Update(float dt);
+//    void Clear();
+//
+//    template<typename TObject>
+//    inline void Add(TObject* userObject, bool(TObject::* callback)(float));
+//
+//private:
+//    std::vector<std::function<bool(float)>> m_tasks;
+//};
+//
+//template<typename TObject>
+//inline void CTaskManager::Add(TObject* userObject, bool(TObject::* callback)(float))
+//{
+//    auto func = std::bind(callback, userObject, std::placeholders::_1);
+//    m_tasks.push_back(func);
+//}
--- Sources/TaskManager.cpp
+++ Sources/TaskManager.cpp
@@ -1,23 +1,103 @@
 #include "TaskManager.h"
 
-bool CTaskManager::HasTasks() const
+bool CTaskCallbackBase::Invoke(float dt, CTaskState* e)
 {
-    return !m_tasks.empty();
-}
-
-void CTaskManager::Update(float dt)
-{
-    if (HasTasks())
-    {
-        auto it = m_tasks.begin();
-        if (!(*it)(dt))
-        {
-            m_tasks.erase(it);
-        }
-    }
+    return InternalCall(dt, e);
 }
 
 void CTaskManager::Clear()
 {
-    m_tasks.clear();
+    auto it = m_taskList.begin();
+    while (it != m_taskList.end())
+    {
+        m_taskToDelete.push_back(*it);
+        it++;
+    }
 }
+
+void CTaskManager::Update(float dt)
+{
+    auto it = m_taskList.begin();
+    while (it != m_taskList.end())
+    {
+        bool r = (*it)->Update(dt);
+
+        if (!r)
+        {
+            it = m_taskList.erase(it);
+        }
+        else
+        {
+            it++;
+        }
+    }
+
+    if (m_taskToDelete.size() > 0)
+    {
+        auto it = m_taskToDelete.begin();
+        while (it != m_taskToDelete.end())
+        {
+            auto taskIt = m_taskList.begin();
+            while (taskIt != m_taskList.end())
+            {
+                if (*it == *taskIt)
+                {
+                    taskIt = m_taskList.erase(taskIt);
+                }
+                else
+                {
+                    taskIt++;
+                }
+            }
+
+            delete *it;
+            it++;
+        }
+
+        m_taskToDelete.clear();
+    }
+}
+
+bool CTaskManager::HasTasks() const
+{
+    return m_taskList.size() > 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+//bool CTaskManager::HasTasks() const
+//{
+//    return !m_tasks.empty();
+//}
+//
+//void CTaskManager::Update(float dt)
+//{
+//    if (HasTasks())
+//    {
+//        auto it = m_tasks.begin();
+//        if (!(*it)(dt))
+//        {
+//            if (it != m_tasks.end())
+//            {
+//                m_tasks.erase(it);
+//            }
+//        }
+//    }
+//}
+//
+//void CTaskManager::Clear()
+//{
+//    m_tasks.clear();
+//}
