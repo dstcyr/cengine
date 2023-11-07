7e84065a616e40432ca1c82b434cfda6390418c2
David St-Cyr <st-cyrdavid@hotmail.com>
2023-11-06 21:39:59 -0500

    Adding vector function

--- Includes/VectorHelper.h
+++ Includes/VectorHelper.h
@@ -10,4 +10,21 @@ namespace Engine
     {
         return std::count(v.begin(), v.end(), object) > 0;
     }
+
+    template<typename T>
+    void Remove(std::vector<T*>& v, T* object)
+    {
+        auto it = v.begin();
+        while (it != v.end())
+        {
+            if (*it == object)
+            {
+                it = v.erase(it);
+            }
+            else
+            {
+                ++it;
+            }
+        }
+    }
 }
